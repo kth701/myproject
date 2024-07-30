e428f6462fd77027fdb445ade24084ec0e6bfd60
kthnaver <kth_701@naver.com>
2024-07-30 10:29:13 +0900

    - board entity, dto

---
+++ src/main/java/com/spring/myproject/dto/BoardDTO.java
@@ -0,0 +1,13 @@
+package com.spring.myproject.dto;
+
+
+import java.time.LocalDateTime;
+
+public class BoardDTO {
+  private Long bno;
+  private String title;
+  private String content;
+  private String writer;
+  private LocalDateTime regDate;
+  private LocalDateTime modDdate;
+}
---
+++ src/main/java/com/spring/myproject/dto/ReplyDTO.java
@@ -0,0 +1,28 @@
+package com.spring.myproject.dto;
+
+import io.swagger.v3.oas.annotations.callbacks.Callback;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReplyDTO {
+  private Long rno;
+
+  // spring-boot-starter-validation: 서버 유효성 검사 라이브러리
+  @NotNull
+  private Long bno;// 댓글의 부모
+  @NotEmpty
+  private String replyText;
+  @NotEmpty
+  private String replyer;
+
+  private LocalDateTime regDate, modDate;
+}
