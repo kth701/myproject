144308aaca55470f6b30b4511ee252cccbf5af4a
kthnaver <kth_701@naver.com>
2024-07-30 18:25:09 +0900

    - thymleaf template

--- src/main/java/com/spring/myproject/config/CustomServletConfig.java
+++ src/main/java/com/spring/myproject/config/CustomServletConfig.java
@@ -3,7 +3,7 @@ package com.spring.myproject.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@Configuration
-@EnableWebMvc
+//@Configuration
+//@EnableWebMvc
 public class CustomServletConfig {
 }
---
+++ src/main/java/com/spring/myproject/controller/BoardController.java
@@ -0,0 +1,36 @@
+package com.spring.myproject.controller;
+
+
+import com.spring.myproject.dto.PageRequestDTO;
+import com.spring.myproject.dto.PageResponseDTO;
+import com.spring.myproject.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller@Log4j2
+@RequestMapping("/board")
+@RequiredArgsConstructor
+public class BoardController {
+  private final BoardService boardService;
+
+
+  @GetMapping("/root")
+  public String root(){
+    return "index";
+  }
+
+  @GetMapping("/list")
+  public String list(PageRequestDTO pageRequestDTO, Model model){
+    // PageRequestDTO 객체 생성만 했을 경우 기본값 설정
+
+    PageResponseDTO responseDTO = boardService.list(pageRequestDTO);
+    log.info("=> "+responseDTO);
+
+    model.addAttribute("responseDTO", responseDTO);
+    return "board/list";
+  }
+}
