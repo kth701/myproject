866c289b84f10ac8f0af350dd7a3eb8773198eb8
kthnaver <kth_701@naver.com>
2024-07-31 18:23:36 +0900

    - board register

--- src/main/java/com/spring/myproject/controller/BoardController.java
+++ src/main/java/com/spring/myproject/controller/BoardController.java
@@ -40,28 +40,48 @@ public class BoardController {
     return "board/list";
   }
   // 2. 게시글 등록
+  @GetMapping("/register")
+  public String registerGet(){
+    // 게시글 등록 입력 폼 요청
+    return "board/register";
+  }
   @PostMapping("/register")
   public String registerPost(@Valid BoardDTO boardDTO,
                              BindingResult bindingResult,
                              RedirectAttributes redirectAttributes){
 
+    // 클라이언트로 부터 전송받은 boardDTO를 @Valid에서 문제가 발생했을 경우
     if (bindingResult.hasErrors()){
       log.info("=> has errors...");
 
       // 1회용 정보유지 : redirect방식으로 요청시 정보관리하는 객체
       redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
-      return "redirect:/board/register";
+      return "redirect:/board/register"; // Get방식으로 /board/register재요청
     }
     log.info("=> "+boardDTO);
-    // 게시글 등록 서비스 호출
+
+
+    // 게시글 등록 서비스 호출(DB에 저장)
     Long bno = boardService.register(boardDTO);
 
+    // db처리한 후 결과값을 객체에 저장
     redirectAttributes.addFlashAttribute("result", bno);
-
     return "redirect:/board/list";
   }
 
   // 3. 게시글 조회
+  @GetMapping("/read")
+  public void read(Long bno,
+                   PageRequestDTO pageRequestDTO,
+                   Model model){
+
+    // 게시글 조회 서비스 요청
+    BoardDTO boardDTO = boardService.readOne(bno);
+    model.addAttribute("dto", boardDTO);
+
+    // 반환값을 void할경우 url요청경로을 기준으로
+    // return "board/read" 형태로 자동 포워딩됨
+  }
 
   // 4. 게시글 수정
 
