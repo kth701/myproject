392153a7767aaa51c6e01a244cb56b0b2115e076
kthnaver <kth_701@naver.com>
2024-08-07 18:01:19 +0900

    -

--- src/test/java/com/spring/myproject/service/MemberServiceTest.java
+++ src/test/java/com/spring/myproject/service/MemberServiceTest.java
@@ -2,6 +2,7 @@ package com.spring.myproject.service;
 
 import com.spring.myproject.dto.MemberDTO;
 import com.spring.myproject.entity.Member;
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,7 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @Transactional
 @TestPropertySource(locations = {"classpath:application-test.properties"})
+@Log4j2
 class MemberServiceTest {
 
   @Autowired
@@ -23,7 +25,7 @@ class MemberServiceTest {
   PasswordEncoder passwordEncoder;
 
   // 회원 정보 DTO, Entity생성하기
-  public Member createMember(){
+  public MemberDTO createMember(){
     // 클라이언트로부터 전달받은
     // 더미 data MemberDTO 생성
     MemberDTO memberDTO = MemberDTO.builder()
@@ -41,7 +43,8 @@ class MemberServiceTest {
     //return Member.createMember(memberDTO, passwordEncoder);
 
     // 2. dto->entity :인터페이스에서 정의한 메서드 활용
-    return memberService.dtoToEntity(memberDTO, passwordEncoder);
+    //return memberService.dtoToEntity(memberDTO, passwordEncoder);
+    return memberDTO;
   }
 
   @Test
@@ -49,8 +52,16 @@ class MemberServiceTest {
   public void saveDuplicateMemberTest(){
 
     // 회원1
-    Member member = createMember();
-    Member savedMember = memberService.saveMember(member);
+    MemberDTO memberDTO = createMember();
+
+    // 1. dto -> entity: Member Entity createMember() 메서드 활용
+    //Member member = Member.createMember(memberDTO, passwordEncoder);
+
+    // 2. dto -> entity: MeberService 인터페이스 활용
+    // save전 entity
+    Member member = memberService.dtoToEntity(memberDTO, passwordEncoder );
+    // save후 entity
+    Member savedMember = memberService.saveMember(memberDTO);
 
     // 회원 등록 테스트 결과 체크: assertEquals(기대값, 실제값)
     assertEquals(member.getEmail(),     savedMember.getEmail());
@@ -64,19 +75,24 @@ class MemberServiceTest {
   @Test
   @DisplayName("중복 회원 가입 테스트")
   public void saveMemberTest(){
-    // 회원1
-    Member member1 = createMember();
-    memberService.saveMember(member1);
-    // 회원2
-    Member member2 = createMember();
 
+    // 회원1
+    MemberDTO memberDTO1 = createMember();
+    memberService.saveMember(memberDTO1 );
+
+    // 회원2
+    MemberDTO memberDTO2 = createMember();
+
+    // assertThrows(예외 발생 타입, 실제 예외발생) 메서드: 예외 처리 테스트 메서드
+    // 중복된 이메일 회원등록시 예외발생시 객체 생성
     Throwable e = assertThrows(
-        IllegalStateException.class, () ->{  memberService.saveMember(member2); }
+        // 예외 발생 타입, 실제 예외발생
+        IllegalStateException.class, () ->{  memberService.saveMember(memberDTO2);; }
     );
 
     // 예외 발생 메시지 동일 여부 확인
     assertEquals("이미 가입된 회원 입니다.",      e.getMessage());
-
+    log.info("=> e.getMessage():"+e.getMessage());
   }
 
 }
